510508aa69d86ac61cc9ce36bb9ef71ffefa7c51
21Jeet1996 <jaiswaramarjeet19@gmail.com>
2025-05-13 23:25:28 +0530

    addedd header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProductHeaderComponent } from "./components/product-header/product-header.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  standalone: true,
+  imports: [RouterOutlet, ProductHeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/product-header/product-header.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  standalone: true,
+  selector: 'app-product-header',
+  imports: [RouterModule],
+  templateUrl: './product-header.component.html',
+  styleUrl: './product-header.component.css'
+})
+export class ProductHeaderComponent {
+
+}
